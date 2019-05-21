f8f0d46e3a1abd5c76af6cffc91a92d5e9e3b54c
Jupraj <juprajsngh1@gmail.com>
2019-05-20 22:31:10 -0400

    Added Setter Injection with Annotations by including autowired

--- com/Luv2code/springdemo/AnnotationDemoApp.java
+++ com/Luv2code/springdemo/AnnotationDemoApp.java
@@ -12,12 +12,12 @@ public class AnnotationDemoApp {
 		
 		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
 		// call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		//call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
-		
 		
 		// close the context
 		context.close();
