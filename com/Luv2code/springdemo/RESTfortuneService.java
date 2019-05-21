a3885ef38973c263a02cd24d706c4ee006f22809
Jupraj <juprajsngh1@gmail.com>
2019-05-20 23:25:13 -0400

    Added Qualifiers for Dependency Injection inside happyFortuneService

---
+++ com/Luv2code/springdemo/RESTfortuneService.java
@@ -0,0 +1,13 @@
+package com.Luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component 
+public class RESTfortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
--- com/Luv2code/springdemo/TennisCoach.java
+++ com/Luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.Luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
 	@Autowired 
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService; // This is field injection with the @Autowired above it
 	
 	
