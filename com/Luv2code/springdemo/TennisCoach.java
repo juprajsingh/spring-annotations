c134b4409f9edd51e1291dc39b294ec7aeffddab
Jupraj <juprajsngh1@gmail.com>
2019-05-22 14:10:32 -0400

    Added java config without xml, check JavaConfigDemo class

--- com/Luv2code/springdemo/TennisCoach.java
+++ com/Luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,16 @@
 package com.Luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired 
@@ -21,6 +23,20 @@ public class TennisCoach implements Coach {
 	public TennisCoach() {
 		System.out.println(">> tennisCoach: inside default constructor");
 	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: Inside of doMyCleanupStuff");
+	}
+	
+	
 	
 	/*
 	@Autowired
