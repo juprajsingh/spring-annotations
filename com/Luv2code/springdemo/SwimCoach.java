f4b152bbb151508a937dfcffb6ef8030a37b800d
Jupraj Singh <juprajsingh1@gmail.com>
2019-05-23 18:02:53 -0400

    injected property values from file, check SwimJavaConfigDemo, sport.properties, SwimCoach, and SportConfig

--- com/Luv2code/springdemo/SwimCoach.java
+++ com/Luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,16 @@
 package com.Luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}") 	// this is field level injection
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
@@ -17,5 +25,14 @@ public class SwimCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
 
+	
 }
--- com/Luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ com/Luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -1,24 +1,28 @@
 package com.Luv2code.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SwimJavaConfigDemoApp {
 // CHANGING SOMETHING FOR GIT HUB
 	public static void main(String[] args) {
 		
-		//read spring  config java class
+		// read spring  config java class
 		AnnotationConfigApplicationContext context =
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		// get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
 		
-		//call method to get the daily fortune
+		// call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
 		
+		// call our new swim coach methods ... has the property values injected
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
+
 		// close the context
 		context.close();
 		
